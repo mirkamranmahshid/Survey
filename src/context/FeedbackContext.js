831985c5df2c369377673ec4bee3f79a8ca1a947
mirkamranmahshid <secure.communicatione@gmail.com>
2023-06-11 01:15:10 +0430

    first commit

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,34 +1,40 @@
-import { createContext, useState} from "react"
-import {v4 as uuidv4} from 'uuid'
-import feedbackData from '../data/FeedbackData'
+import { createContext, useState, useEffect} from "react"
+//import {v4 as uuidv4} from 'uuid'
+//import feedbackData from '../data/FeedbackData'
 const FeedbackContext = createContext()
-//import { useEffect } from "react"
+
 export const FeedbackProvider =({children}) => {
-    //const [isLoading, setIsLoading]= useState(true)
-    const [feedback, setFeedback]=useState(feedbackData)
+    const [isLoading, setIsLoading]= useState(true)
+    const [feedback, setFeedback]=useState([])
     const [feedbackEdit, setFeedbackEdit]=useState({
         item:{}, 
         edit:false
     })
-//useEffect(()=>{  fetchFeedback()   },[])
+useEffect(()=>{  fetchFeedback()   },[])
 //Fetch feedback
-/* const fetchFeedback = async()=>{
-    const response =await fetch(`http://localhost:5000/feedback?_sort=id$_order=desc`)
+ const fetchFeedback = async()=>{
+    const response =await fetch(`https://my-json-server.typicode.com/mirkamranmahshid/survey/feedback?_sort=id$_order=desc`)
     const data= await response.json()
     setFeedback(data)
     setIsLoading(false)
-} */
+} 
 //Delete feedback    
-        const deleteFeedback = (id) =>{
+        const deleteFeedback =async (id) =>{
             if(window.confirm('این دیدگاه ثبت شده است آیا از پاک کردن آن اطمینان دارید؟')){
                 if(id<Math.ceil(feedback.length/2)+Math.ceil(feedback.length/3)+1) {window.alert('You need administrator permission ....\n نیاز به مجوز مدیر دارید')} 
                  else {
+                 await fetch(`/feedback/$(id)`, {method:'DELETE' })   
                 setFeedback(feedback.filter((item) => item.id !==id))}
                                                                                                }  
                                       }
-                          const addFeedback = (newFeedback) => { 
-                                 newFeedback.id = uuidv4()
-                                  setFeedback([newFeedback, ...feedback])
+                          const addFeedback = async (newFeedback) => { 
+                            const response =await fetch('/feedback', {
+                                method: 'POST',
+                                headers: { 'Content-Type': 'application/json'},
+                                body: JSON.stringify(newFeedback)
+                            })
+                                 const data =await response.json()
+                                  setFeedback([data, ...feedback])
                                                                 } 
                                                     //set item to be updated                                   
                                              const editFeedback= (item) =>{
@@ -38,12 +44,17 @@ export const FeedbackProvider =({children}) => {
                                                                   })
                                                                           } 
                                 //Update feedback
-                      const updateFeedback =(id, updItem)=>{
-                        setFeedback(feedback.map((item)=>item.id===id ? {...item, ...updItem} : item ))
+                      const updateFeedback =async (id, updItem)=>{ const response= await fetch(`/feedback/${id}`, {
+                        method: 'PUT',
+                        headers: {'Content-Type':'application/json'},
+                        body: JSON.stringify(updItem)
+                      })
+                      const data= await response.json()
+                        setFeedback(feedback.map((item)=>item.id===id ? {...item, ...data} : item ))
                       }
         return <FeedbackContext.Provider  value={{
 feedback,
-//isLoading,
+isLoading,
 feedbackEdit,
 deleteFeedback,
 addFeedback,
